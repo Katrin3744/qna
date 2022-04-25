90de14bcbc8cb1ada1ff0d53d740603960d68f79
Katrin3744 <k.kukh3477@gmail.com>
2022-04-25 16:34:49 +0300

    answer test for create and create method in controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :find_question, only: [:index, :new]
+  before_action :find_question, only: [:index, :new, :create]
   def index
     @answers = @question.answers
   end
@@ -12,9 +12,23 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new
   end
 
+  def create
+    @answer = @question.answers.build(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+
+    params.require(:answer).permit(:body)
   end
 end
