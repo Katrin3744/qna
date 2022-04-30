9ef041f611c824e3ef7808143b15e6317a3ec5a9
Katrin3744 <k.kukh3477@gmail.com>
2022-04-30 20:27:26 +0300

    user can view list of questions

---
+++ spec/features/question/view_list_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User can view the list of question', %q{
+  In order to explore all questions from a community
+  I'd like to be able to view the list of question
+} do
+
+  scenario 'view list of questions' do
+    visit questions_path
+    expect(page).to have_content 'All questions'
+  end
+
+end
