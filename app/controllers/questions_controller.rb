b3ce5cb4b3928d8bbb87c9a75fa3d907e03f96e8
Katrin3744 <k.kukh3477@gmail.com>
2022-04-30 20:21:04 +0300

    user can create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,7 +14,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
     end
