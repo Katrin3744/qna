f7e9b8a77c82eab09608b2d624c6d8cc708df71b
Katrin3744 <k.kukh3477@gmail.com>
2022-05-14 21:33:59 +0300

    create user's sign out with test

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to exit
+  As an unauthenticated user
+  I'd like to be able to sign out
+} do
+
+  given(:user) {create(:user)}
+
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit root_path
+    end
+
+    scenario 'User tries to sign out' do
+      click_on 'Exit'
+
+      expect(page).to have_content 'Signed out successfully.'
+    end
+  end
+end
